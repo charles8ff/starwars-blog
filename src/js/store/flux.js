540cf2db95759a783feb50ed6f8cb1c104efb12e
jagtrrz <jagutierrezc7@gmail.com>
2021-02-03 11:54:25 +0000

    planets cards painted

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,26 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			planets: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			setPlanets: listPlanets => {
+				console.log(listPlanets);
+				setStore({ planets: listPlanets });
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getPlanets: () => {
+				fetch("https://www.swapi.tech/api/planets")
+					.then(response => {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(responseAsJson => {
+						getActions().setPlanets(responseAsJson.results);
+						console.log(responseAsJson);
+					});
 			}
 		}
 	};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,23 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect, useContext } from "react";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+import { Context } from "../store/appContext";
+import { Cardplanets } from "../component/card.jsx";
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	console.log("estoy en la home", store.planets);
+	const urlImg =
+		"https://images.unsplash.com/photo-1545156521-77bd85671d30?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80";
+
+	let listPlanets = store.planets.map((item, index) => {
+		return <Cardplanets name={item.name} src={urlImg} key={index} />;
+	});
+	console.log("adios", listPlanets);
+
+	return (
+		<content className="container">
+			<div className="row d-flex flex-row justify-content-between">{listPlanets}</div>
+		</content>
+	);
+};
