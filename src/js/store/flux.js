45ca1c882e03264d7481b3a17093e2cc0e91af2a
Carlos Fisac Ferrández <charles8ff@gmail.com>
2021-02-03 11:58:00 +0000

    cards from starships

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,48 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			starShipsList: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			// exampleFunction: () => {
+			// 	getActions().changeColor(0, "green");
+			// },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getStarShips: () => {
+				fetch("https://www.swapi.tech/api/starships")
+					.then(response => {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json(); // Read the response as json.
+					})
+					.then(responseAsJson => {
+						// Do stuff with the JSON
+						console.log("starships", responseAsJson);
+						getActions().setStarShips(responseAsJson.results);
+					})
+					.catch(error => {
+						console.log("Looks like there was a problem: \n", error);
+					});
+			},
+			setStarShips: starShipsfromAPI => {
+				setStore({ starShipsList: starShipsfromAPI });
 			}
+			// changeColor: (index, color) => {
+			// 	//get the store
+			// 	const store = getStore();
+
+			// 	//we have to loop the entire demo array to look for the respective index
+			// 	//and change its color
+			// 	const demo = store.demo.map((elm, i) => {
+			// 		if (i === index) elm.background = color;
+			// 		return elm;
+			// 	});
+
+			// 	//reset the global store
+			// 	setStore({ demo: demo });
+			// }
 		}
 	};
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,27 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 import "../../styles/home.scss";
+import CardDeck from "react-bootstrap/Card";
+import CardStarShips from "../component/cardstarships.jsx";
+export const Home = () => {
+	const { store, actions } = useContext(Context);
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+	console.log("from store", store.starShipsList);
+
+	let starShipsInHTML = store.starShipsList.map((elem, index) => {
+		return (
+			<CardStarShips
+				key={index}
+				name={elem.name}
+				imgDefault="https://i.pinimg.com/originals/f4/4d/7f/f44d7f40b15cef45e9d6fe14cad7fb79.jpg"
+			/>
+		);
+	});
+	console.log("in html", starShipsInHTML);
+
+	return (
+		<div className="container text-center mt-5">
+			<div className="row d-flex flex-row justify-content-between">{starShipsInHTML}</div>
+		</div>
+	);
+};
