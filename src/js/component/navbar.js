78cb608789f20c125ef06c0593895620df46c69a
danseks <danseksfzz@gmail.com>
2021-02-07 11:41:57 +0000

    Change navbar and some styles

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
 
 export const Navbars = () => {
 	return (
-		<Navbar className="navbar">
-			<Navbar.Brand href="#home">
-				<img
-					alt=""
-					src="https://www.unioncosmos.com/wp-content/uploads/2017/01/Union-Cosmos-Star-Wars-Logo-Amarillo-PNG.png"
-					width="130"
-					height="50"
-					className="d-inline-block align-top"
-				/>{" "}
-			</Navbar.Brand>
-		</Navbar>
+		<>
+			<Navbar className="navbar-css" expand="lg">
+				<Container>
+					<Navbar.Brand href="#home">
+						<img
+							alt=""
+							src="https://www.unioncosmos.com/wp-content/uploads/2017/01/Union-Cosmos-Star-Wars-Logo-Amarillo-PNG.png"
+							width="130"
+							height="50"
+							className="d-inline-block align-top"
+						/>{" "}
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="ml-auto">
+							<Nav.Link href="#home">Home</Nav.Link>
+							<Nav.Link href="#link">Link</Nav.Link>
+							<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+								<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+						<Form inline>
+							<FormControl type="text" placeholder="Search" className="mr-sm-2" />
+							<Button variant="outline-success">Search</Button>
+						</Form>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</>
 	);
 };
